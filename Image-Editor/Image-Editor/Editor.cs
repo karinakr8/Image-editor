777333ba0deae688b257c2520a4629d3eb75f805
karinakr8 <karinakra802@gmail.com>
2022-04-25 22:51:02 +0300

    Added a file getter to a constructor and created method that gets all files of declared filters from the folder.

--- Image-Editor/Image-Editor/Editor.cs
+++ Image-Editor/Image-Editor/Editor.cs
@@ -16,14 +16,25 @@ namespace Image_Editor
 		// take folder, path and resize all the images in it
 		public Editor(string folder)
 		{
-			
+			this.files = GetFilesFrom(folder, filters, false);
 		}
 
 		public void Resize_Image(int minCoord)
 		{
 			Console.WriteLine("Resizing...");
-		
+
 			Console.WriteLine("Resized.");
+		}
+
+		public static String[] GetFilesFrom(String searchFolder, String[] filters, bool isRecursive)
+		{
+			List<String> filesFound = new List<String>();
+			var searchOption = isRecursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+			foreach (var filter in filters)
+			{
+				filesFound.AddRange(Directory.GetFiles(searchFolder, String.Format("*.{0}", filter), searchOption));
+			}
+			return filesFound.ToArray();
 		}
 	}
 }
