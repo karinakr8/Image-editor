23c2f8e7cf84b035946e4a89e671ca0d177fc1e0
karinakr8 <karinakra802@gmail.com>
2022-04-25 22:56:19 +0300

    Changed Program class by adding read from file logic (file containing path to a folder with images). Also added Resize_Image reference to Main, so now the program can resize images.

--- Image-Editor/Image-Editor/Program.cs
+++ Image-Editor/Image-Editor/Program.cs
@@ -4,8 +4,32 @@
 	{
 		static void Main(string[] args)
 		{
+			// create file if not created
+			const string textFile = "path.txt";
+			const int maxCoord = 800;
+			string mainFolder = Directory.GetCurrentDirectory();
 			Editor editor;
+
+			if (!File.Exists(textFile))
+			{
+				File.CreateText(textFile);
+				Console.WriteLine("Write the path to the image folder into the \"path.txt\" file.");
+				return;
+			}
+
+			// read path from file
+			string mainFolderFromText = File.ReadAllText(textFile);
+
+			if (mainFolderFromText == "")
+			{
+				editor = new Editor(mainFolder);
+			}
+			else
+			{
+				editor = new Editor(mainFolderFromText);
+			}
+
+			editor.Resize_Image(maxCoord);
 		}
 	}
 }
-
