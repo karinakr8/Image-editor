3e7accefc776f87e0d67c51a92cfe5a781755925
karinakr8 <karinakra802@gmail.com>
2022-04-25 22:50:19 +0300

    Added a constructor for Editor class and a skeleton of Image-Editor program.

--- Image-Editor/Image-Editor/Editor.cs
+++ Image-Editor/Image-Editor/Editor.cs
@@ -10,6 +10,20 @@ namespace Image_Editor
 {
     internal class Editor
     {
+		private readonly String[] filters = new String[] { "jpg", "jpeg", "png" };
+		public String[] files;
 
-    }
+		// take folder, path and resize all the images in it
+		public Editor(string folder)
+		{
+			
+		}
+
+		public void Resize_Image(int minCoord)
+		{
+			Console.WriteLine("Resizing...");
+		
+			Console.WriteLine("Resized.");
+		}
+	}
 }
