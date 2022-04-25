2cdd954b4d8586aaa7aec0a07592abcdb29cfc76
karinakr8 <karinakra802@gmail.com>
2022-04-25 22:41:03 +0300

    Created project template.

---
+++ Image-Editor/Image-Editor/Editor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Imaging;
+using System.Drawing;
+
+namespace Image_Editor
+{
+    internal class Editor
+    {
+
+    }
+}
