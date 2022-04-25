250753fad18bb8eebe869d519d7fb905013114c1
karinakr8 <karinakra802@gmail.com>
2022-04-25 22:54:06 +0300

    Added image resizing logic to Editor class. At the end of Resize_Image method program now resaves new images while deleting old ones.

--- Image-Editor/Image-Editor/Editor.cs
+++ Image-Editor/Image-Editor/Editor.cs
@@ -22,8 +22,48 @@ namespace Image_Editor
 		public void Resize_Image(int minCoord)
 		{
 			Console.WriteLine("Resizing...");
+			for (int i = 0; i < files.Length; i++)
+			{
+				// load image
+				Bitmap? cloneImg = null;
+				using (Bitmap bitMapImage = new(files[i]))
+				{
+					cloneImg = new Bitmap(bitMapImage);
+				}
+				// resize image
+				Image resized = Resize(cloneImg, minCoord);
 
+				// save image
+				File.Delete(files[i]);
+				resized.Save(files[i], ImageFormat.Jpeg);
+			}
 			Console.WriteLine("Resized.");
+		}
+
+		public static Bitmap Resize(Image img, int coord)
+		{
+			Size size = new Size();
+
+			// calculate size with respect
+			decimal height = img.Height;
+			decimal width = img.Width;
+
+			if (height > 0 && width > 0)
+			{
+				height = coord / height;
+				width = coord / width;
+			}
+
+			// create an image of the desired size
+			if (img.Width > img.Height)
+			{
+				size = new Size((int)(img.Width * height), coord);
+			}
+			else
+			{
+				size = new Size(coord, (int)(img.Height * width));
+			}
+			return new Bitmap(img, size);
 		}
 
 		public static String[] GetFilesFrom(String searchFolder, String[] filters, bool isRecursive)
